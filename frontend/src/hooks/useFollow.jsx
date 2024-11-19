6b31535c86bc2c10f31526e39801ebe8e876f3ea
Edison Liang <edisonliang@lev-12476.apn.wlan.private.upenn.edu>
2024-11-19 03:23:54 -0500

    finish profile updating

--- frontend/src/hooks/useFollow.jsx
+++ frontend/src/hooks/useFollow.jsx
@@ -7,7 +7,7 @@ const useFollow = () => {
     const {mutate: follow, isPending} = useMutation({
         mutationFn: async(userId) => {
             try {
-                const res = await fetch(`api/users/follow/${userId}`, {
+                const res = await fetch(`/api/users/follow/${userId}`, {
                     method: "POST",
                 })
 
@@ -22,7 +22,7 @@ const useFollow = () => {
             // Refresh suggestedUsers
             Promise.all([
                 queryClient.invalidateQueries({queryKey: ["suggestedUsers"]}),
-                queryClient.invalidateQueries({queryKey: ["authUsers"]})
+                queryClient.invalidateQueries({queryKey: ["authUser"]})
             ])
         },
         onError: (error) => {
