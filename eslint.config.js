d5c6b3c16a7b4c1dc0ff875dfc8e8f6dd9b8a02a
Edison Liang <edisonliang@lev-12476.apn.wlan.private.upenn.edu>
2024-11-18 22:36:59 -0500

    finish ui

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,10 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: {
-      globals: {
-        ...globals.browser, // Include browser globals like `window` and `document`
-        ...globals.node,    // Include Node.js globals like `process` and `__dirname`
-      },
-    },
-  },
-  pluginJs.configs.recommended,
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
   pluginReact.configs.flat.recommended,
 ];
